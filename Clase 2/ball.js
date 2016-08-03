90861b94bc7ba9ad527f8eab9feaa30ded7179db
yacaFx <sergiobritor@gmail.com>
2016-08-02 20:50:50 -0500

    All files for class 2

--- Clase 2/ball.js
+++ Clase 2/ball.js
@@ -1,11 +1,44 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var Ball = (function () {
-    function Ball() {
+    function Ball(name) {
+        this.price = 50;
+        this.name = name;
+        console.log("La pelota cuesta: $ " + this.price);
     }
     Ball.prototype.setBrandName = function (brandName) {
         this.brandName = brandName;
     };
     return Ball;
 }());
-var myBall = new Ball();
+var Rugby = (function (_super) {
+    __extends(Rugby, _super);
+    function Rugby(brandName) {
+        _super.call(this, brandName);
+        console.log(brandName);
+    }
+    return Rugby;
+}(Ball));
+var Calculator = (function () {
+    function Calculator() {
+    }
+    Calculator.add = function (a, b) {
+        return a + b;
+    };
+    Calculator.prototype.minus = function (a, b) {
+        return a - b;
+    };
+    Calculator.value = 123;
+    return Calculator;
+}());
+console.log("Protected: " + Calculator.value);
+console.log("Add: " + Calculator.add(2, 5));
+console.log("Minus: " + Calculator.minus(2, 5));
+var myRugbyBall = new Rugby('Lion ball');
+console.log(myRugbyBall);
+var myBall = new Ball('Aguila');
 myBall.setBrandName('Nike');
 console.log(myBall);
--- Clase 2/ball.ts
+++ Clase 2/ball.ts
@@ -1,12 +1,47 @@
-class Ball{
-  brandName: string;
+class Ball {
+    brandName: string;
+    public name: string;
+    private price: number = 50;
 
-  setBrandName(brandName: string){
-    this.brandName = brandName;
-  }
+    constructor(name: string) {
+        this.name = name;
+        console.log("La pelota cuesta: $ " + this.price)
+    }
+
+    setBrandName(brandName: string) {
+        this.brandName = brandName;
+    }
 }
 
-var myBall = new Ball();
+class Rugby extends Ball {
+    constructor(brandName) {
+        super(brandName)
+        console.log(brandName);
+    }
+}
+
+class Calculator {
+    static value: number = 123;
+
+    static add(a:number, b:number){
+      return a + b;
+    }
+
+    minus(a:number, b:number){
+      return a - b;
+    }
+
+}
+
+console.log("Protected: " + Calculator.value)
+console.log("Add: " + Calculator.add(2,5));
+//console.log("Minus: " + Calculator.minus(2,5));
+
+
+var myRugbyBall = new Rugby('Lion ball');
+console.log(myRugbyBall)
+
+var myBall = new Ball('Aguila');
 myBall.setBrandName('Nike');
 
 console.log(myBall)
